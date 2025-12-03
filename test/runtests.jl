fa655038d7f44e1e8a81c385ab1e3f22644557cb
souta shimozono <souta@panza.hgnet>
2025-12-03 15:07:30 +0000

    fixed test code

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,20 +27,19 @@ const dirs = ["model", "solver"]
 @testset "tests" begin
     test_args = copy(ARGS)
     println("Passed arguments ARGS = $(test_args) to tests.")
-        @time for dir in dirs
-            dirpath = joinpath(@__DIR__, dir)
-            println("\nTest $(dirpath)")
-            # Find all files named test_*.jl in the directory and include them.
-            files = sort(filter(f -> startswith(f, "test_") && endswith(f, ".jl"), readdir(dirpath)))
-            if isempty(files)
-                println("  No test files found in $(dirpath).")
-            else
-                for f in files
-                    filepath = joinpath(dirpath, f)
-                    @time begin
-                        println("  Including $(filepath)")
-                        include(filepath)
-                    end
+    @time for dir in dirs
+        dirpath = joinpath(@__DIR__, dir)
+        println("\nTest $(dirpath)")
+        # Find all files named test_*.jl in the directory and include them.
+        files = sort(filter(f -> startswith(f, "test_") && endswith(f, ".jl"), readdir(dirpath)))
+        if isempty(files)
+            println("  No test files found in $(dirpath).")
+        else
+            for f in files
+                filepath = joinpath(dirpath, f)
+                @time begin
+                    println("  Including $(filepath)")
+                    include(filepath)
                 end
             end
         end
